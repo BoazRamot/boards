03f7410e81ee0e95ee96228107063f284ae3bbf4
BoazRamot <boaz.ramot@gmail.com>
2019-07-13 13:11:14 +0300

    Adding search box and map click event

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { SyntheticEvent, useEffect, useState } from 'react';
 import './App.css';
 import L from 'leaflet';
 
 const App: React.FC = () => {
-  let myMap: any;
+  const [search, setSearch] = useState('');
+  const [buttonEnabled, setButtonEnabled] = useState(false);
+  const [myMap, setMyMap] = useState();
+
+  // let myMap: any;
+
   useEffect(() => {
     // create map
-    myMap = L.map('map', {
+    const map = L.map('map', {
       center: [51.505, -0.09],
       zoom: 13,
       layers: [
@@ -15,6 +20,13 @@ const App: React.FC = () => {
         }),
       ]
     });
+    map.addEventListener('click', (e: any) => {
+      console.log('e', e);
+      console.log('Lat', e.latlng.lat);
+      console.log('Lon', e.latlng.lng);
+      map.setView([e.latlng.lat, e.latlng.lng], 16);
+    });
+    setMyMap(map);
   }, []);
 
   const handleClick = () => {
@@ -27,8 +39,38 @@ const App: React.FC = () => {
     // navigator.geolocation.clearWatch(watchID);
   };
 
+  const handleChange = (event: SyntheticEvent) => {
+    const value = (event.target as HTMLInputElement).value;
+    setSearch(value);
+    setButtonEnabled(value.length > 0)
+  };
+
+  const handleSearch = () => {
+    console.log(search);
+    if (buttonEnabled) {
+      // fetch(`https://nominatim.openstreetmap.org/?q=${search}`)
+      // fetch(`https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=bakery+in+berlin+wedding&format=json&limit=1`)
+      fetch(`https://nominatim.openstreetmap.org/?format=json&addressdetails=1&q=${search}&format=json&limit=1`)
+        .then(res => res.json())
+        .then(myJson => {
+          console.log(myJson);
+          console.log(myJson[0]);
+          let lat = +myJson[0].lat;
+          let lon = +myJson[0].lon;
+          console.log('myMap', myMap);
+          myMap.setView([lat, lon], 12);
+        }).catch((e) => {
+          console.error('Location Fetch failed', e)
+      });
+    }
+    setSearch('');
+    setButtonEnabled(false);
+  };
+
   return (
     <div className="App">
+      <input type="text" value={search} onChange={handleChange}/>
+      <button onClick={handleSearch} disabled={!buttonEnabled}>Search</button>
       <div className="map" id="map"/>
       <button onClick={handleClick}>my location</button>
     </div>
